61b1ebaa29bce59405e51214a0d769e91c22f6b6
Franciscojm13 <franciscojmora13@gmail.com>
2022-07-11 20:05:32 -0400

    Implementando una NavBar y estilos con sass

--- poliester_web/src/App.jsx
+++ poliester_web/src/App.jsx
@@ -1,44 +1,18 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './sass/App.scss'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log("hola");
 
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola, esta es la app web "Poliester" de Francisco Mora</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <NavBar />
+    </div>  
   )
 }
 
